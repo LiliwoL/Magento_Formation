f8f975b1003b95ce9caa3cbec0b37fdec413aeed
LiliwoL <github@liliwol.fr>
2022-03-10 15:14:43 +0000

    Commoit correctif

---
+++ FormationMagento/ChangeCustomerPasswordBackend/view/adminhtml/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/form/components/tab': {
+                'FormationMagento_ChangeCustomerPasswordBackend/js/form/components/tab-mixin': true
+            }
+        }
+    }
+};
